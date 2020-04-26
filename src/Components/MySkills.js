8c9226316e0c648e9f3f0f6d7ef62d6bb0e8eede
Lorenzo Gagliardi <loregagliard@gmail.com>
2020-04-27 01:06:22 +0200

    fix (href myskills to login page for each skill): MySkills.js redirect to login from skills

--- src/Components/MySkills.js
+++ src/Components/MySkills.js
@@ -7,67 +7,67 @@ import 'semantic-ui-css/semantic.css';
 export default class MySkills extends Component {
     render() {
         return(
-            <Grid>
+            <Grid padded={true}>
                 <Grid.Row centered>
                     <Grid.Column>
                         <Card.Group itemsPerRow={3}>
-                            <Card key={'cardtime'} link raised>
+                            <Card key={'cardtime'} link raised href={window.location.origin + '/login'}>
                                 <Card.Content>
                                     <Card.Header textAlign={'center'}>
                                         Time
                                     </Card.Header>
                                 </Card.Content>
                             </Card>
-                            <Card key={'carddrivinglicense'} link raised>
+                            <Card key={'carddrivinglicense'} link raised href={window.location.origin + '/login'}>
                                 <Card.Content>
                                     <Card.Header textAlign={'center'}>
                                         Driving License
                                     </Card.Header>
                                 </Card.Content>
                             </Card>
-                            <Card key={'cardempathy'} link raised>
+                            <Card key={'cardempathy'} link raised href={window.location.origin + '/login'}>
                                 <Card.Content>
                                     <Card.Header textAlign={'center'}>
                                         Empathy
                                     </Card.Header>
                                 </Card.Content>
                             </Card>
-                            <Card key={'otherlanguage'} link raised>
+                            <Card key={'otherlanguage'} link raised href={window.location.origin + '/login'}>
                                 <Card.Content>
                                     <Card.Header textAlign={'center'}>
                                         Other Language
                                     </Card.Header>
                                 </Card.Content>
                             </Card>
-                            <Card key={'cardsignlanguage'} link raised>
+                            <Card key={'cardsignlanguage'} link raised href={window.location.origin + '/login'}>
                                 <Card.Content>
                                     <Card.Header textAlign={'center'}>
                                         Sign Language
                                     </Card.Header>
                                 </Card.Content>
                             </Card>
-                            <Card key={'cardbls'} link raised>
+                            <Card key={'cardbls'} link raised href={window.location.origin + '/login'}>
                                 <Card.Content>
                                     <Card.Header textAlign={'center'}>
                                         BLS
                                     </Card.Header>
                                 </Card.Content>
                             </Card>
-                            <Card key={'cardcoordination'} link raised>
+                            <Card key={'cardcoordination'} link raised href={window.location.origin + '/login'}>
                                 <Card.Content>
                                     <Card.Header textAlign={'center'}>
                                         Coordination
                                     </Card.Header>
                                 </Card.Content>
                             </Card>
-                            <Card key={'cardlisteningskills'} link raised>
+                            <Card key={'cardlisteningskills'} link raised href={window.location.origin + '/login'}>
                                 <Card.Content>
                                     <Card.Header textAlign={'center'}>
                                         Listening Skills
                                     </Card.Header>
                                 </Card.Content>
                             </Card>
-                            <Card key={'cardsocialcare'} link raised>
+                            <Card key={'cardsocialcare'} link raised href={window.location.origin + '/login'}>
                                 <Card.Content>
                                     <Card.Header textAlign={'center'}>
                                         Social Care
